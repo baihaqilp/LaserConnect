09ee505341522eb53fe8038f6b98ee92b5db2e39
baihaqilp <baihaqiluhurpangestika@gmail.com>
2019-12-02 00:11:23 +0700

    first commit

---
+++ Assets/Script/MirrorBehaviorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorBehaviorScript : MonoBehaviour
+{
+    
+    void OnMouseDrag(){
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+        transform.position = objPosition;
+    }
+    
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Debug.Log(Input.touchCount);
+        }
+    }         //Floating point variable to store the player's movement speed.
+
+}
+
