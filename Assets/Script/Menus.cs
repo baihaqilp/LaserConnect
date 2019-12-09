8b186cb81c30cbccfb2a7814b24ebaf57cb58d1a
baihaqilp <baihaqiluhurpangestika@gmail.com>
2019-12-09 13:12:00 +0700

    add BGM & Mute Button

--- Assets/Script/Menus.cs
+++ Assets/Script/Menus.cs
@@ -6,20 +6,14 @@ using UnityEngine.SceneManagement;
 public class Menus : MonoBehaviour
 {   
    public void OnMouseClick(){
-        Application.LoadLevel(1);
+       SceneManager.LoadScene(1);
     }
 
     public void OnLevelClick(){
-        Application.LoadLevel(2);
+        SceneManager.LoadScene(2);
     }
 
-    public void OnGameClick(){
-        Application.LoadLevel(3);
-    }
     public void BackPressed(){
-        Application.LoadLevel(0);
-    }
-    public void FeedbackUser(){
-        Application.LoadLevel(5);
+        SceneManager.LoadScene(0);
     }
 }
